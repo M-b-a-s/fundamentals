db60d87870005058f95fdf11734e44c7281e203a
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2025-10-24 03:25:13 +0100

    Initial commit

---
+++ src/main/java/method/Methods.java
@@ -0,0 +1,17 @@
+package method;
+
+/** Methods are subtasks within a class
+* Methods are used to perform a task
+* Methods are also known as functions
+* If an access modifier is not specified, it is default(only accessible within the same package)
+* Some non-access modifiers are static, final, abstract, synchronized, native, etc.
+* return type is the data type of the value the method returns
+* If a method does not return any value, the return type is void
+*/
+
+public class Methods {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
