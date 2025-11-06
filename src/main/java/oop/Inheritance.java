ae3bff4ebdcac72accdc63abeb75a0e050bf3d58
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2025-11-06 12:03:14 +0100

    Practice inheritance in OOP

---
+++ src/main/java/oop/Inheritance.java
@@ -0,0 +1,15 @@
+package oop;
+
+/**
+ *  Inheritance is a mechanism in which a class can inherit the properties and methods of another class.
+ *  Inheritance is a way to create a new class based on an existing class.
+ *  It allows us to reuse code and create new classes that are similar to existing classes.
+ * */ 
+
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+    }
+
+}
