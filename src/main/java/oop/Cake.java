578580cb18147897132762d32e4667b542b8234a
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2025-11-06 14:22:59 +0100

    Inheritance exercise

---
+++ src/main/java/oop/Cake.java
@@ -0,0 +1,28 @@
+package oop;
+
+public class Cake {
+
+    protected String flavor;
+    protected int price;
+
+    public Cake(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+}
